4b53ca817616aaf9a4ec3bcb050f30d07b49f014
Кирилл Черевко <kirillcherevko@gmail.com>
2020-08-06 22:30:08 +0400

    init

---
+++ js/lazyLoadPhotos.js
@@ -0,0 +1,41 @@
+﻿function lazyLoad() {
+    document.addEventListener("DOMContentLoaded", function () {
+        var lazyloadImages = document.querySelectorAll("img.lazy");
+        var lazyloadThrottleTimeout;
+
+        function lazyload() {
+            if (lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+            }
+
+            lazyloadThrottleTimeout = setTimeout(function () {
+                var scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function (img) {
+                    if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                        img.src = img.dataset.src;
+                        img.classList.remove('lazy');
+                    }
+                });
+                if (lazyloadImages.length == 0) {
+                    document.removeEventListener("scroll", lazyload);
+                    window.removeEventListener("resize", lazyload);
+                    window.removeEventListener("orientationChange", lazyload);
+                }
+            }, 20);
+        }
+
+        document.addEventListener("scroll", lazyload);
+        window.addEventListener("resize", lazyload);
+        window.addEventListener("orientationChange", lazyload);
+    });
+};
+
+function loadPhotos() {
+    var url = "https://api.vk.com/method/photos.get?access_token=1867af7e1867af7e1867af7e3318161c29118671867af7e46d528fe6ebfd81c41d67cf1&v=5.103&owner_id=-24735760&album_id=179528153&rev=0&extended=0&feed_type=photo&count=100";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    var jsonContent = xmlHttp.responseText;
+    var content = JSON.parse(json_string);
+    var t = 1;
+}
---
+++ js/site.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../lib/jquery/dist/jquery.js" />
+
+$(function () {
+    var location = window.location.href,
+        cur_url = (location.split('/').pop());
+    if (cur_url === "") {
+        cur_url = "AboutUs"
+    };
+
+    $('.menu-main a').each(function () {
+        var link = $(this).attr('href').split('/').pop();
+
+        if (cur_url == link) {
+            $(this).addClass('current');
+        }
+    });
+});
+
+function load(path) {
+    $('main.content').load(path);
+};
